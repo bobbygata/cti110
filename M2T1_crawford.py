8b5af5b117c8940d4d29d9837517428f227aae86
bobbygata <31699713+bobbygata@users.noreply.github.com>
2017-09-06 11:09:51 -0400

    Add files via upload
    First submission

---
+++ M2T1_crawford.py
@@ -0,0 +1,34 @@
+# CTI 110
+# M2T1 - Sales Prediction
+# 8/23
+# Crawford
+
+# suggested variables: totalSales, annualProfit
+
+# ask the user for total sales
+totalSales = int (input ("The total sale is: "))
+
+# calculate the profit amount
+annualProfit = 0.23 * totalSales
+
+# print the profit amount
+print ("The annual profit is", annualProfit)
+
+
+
+
+'''
+# program to add two numbers
+# userTyped = input ("Type a number:  ")
+# userNumber = int (usertyped)
+
+# get the numbers
+firstNumber = int (input ("Type a number: "))
+secondNumber = int (input ("type another number: "))
+
+# add them
+answer = firstNumber = secondNumber
+
+#print the result
+print ("the answer is", answer)
+'''
