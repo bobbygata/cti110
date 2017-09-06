64d90d9e2844d78be792f8bb87f8785358a9b4ea
bobbygata <31699713+bobbygata@users.noreply.github.com>
2017-09-06 11:50:58 -0400

    Add files via upload
    First submission

---
+++ M2HW1_Crawford.py
@@ -0,0 +1,27 @@
+# CTI-110
+# M2HW1 - Distance Traveled
+# Robert Crawford
+# 9/6/2017
+
+# speed, distanceAfter6, distanceAfter10, distanceAfter15
+
+speed = 70
+
+time = 6
+
+time2 = 10
+
+time3 = 15
+
+distanceAfter6 = speed * time
+
+distanceAfter10 = speed * time2
+
+distanceAfter15 = speed * time3
+
+
+print ("The total miles at 6 hour is" , distanceAfter6)
+
+print ("The total miles at 10 hour is" , distanceAfter10)
+
+print ("The total miles at 15 hour is" , distanceAfter15)
